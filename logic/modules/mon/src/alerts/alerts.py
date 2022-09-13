e73fa083f6608baedcd6c471cf570229a379d679
cesarcajas <mrc.cajas.p@hotmail.com>
2022-09-13 17:24:38 +0200

    mongo connection fixed

--- logic/modules/mon/src/alerts/alerts.py
+++ logic/modules/mon/src/alerts/alerts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Copyright 2021-present i2CAT
@@ -9,8 +9,9 @@ import pymongo
 
 db_manager = DBManager()
 myclient = pymongo.MongoClient(
-    "mongodb://{}:{}".format(db_manager.host, db_manager.port)
+    "mongodb://{}:{}@{}:{}".format(db_manager.user_id, db_manager.user_password, db_manager.host, db_manager.port)
 )
+
 db = myclient[db_manager.db_name]
 alerts_metrics = db["alerts_metrics"]
 
@@ -18,17 +19,17 @@ class Alerts():
     """
     Retrieve list of registered alerts
     """
-    def retrieve_registered_alerts(self, vnf_id, metric_name):
-        if (vnf_id is None) and (metric_name is None):
+    def retrieve_registered_alerts(self, xnf_id, metric_name):
+        if (xnf_id is None) and (metric_name is None):
             mongo_list = []
             for i in alerts_metrics.find():
                 mongo_dict = {
-                    "vnf-id": "",
+                    "xnf-id": "",
                     "metric-name": "",
                     "metric-command": "",
                     "data": "",
                     }
-                mongo_dict["vnf-id"] = i["vnf_id"]
+                mongo_dict["xnf-id"] = i["xnf_id"]
                 mongo_dict["metric-name"] = i["metric_name"]
                 mongo_dict["metric-command"] = i["metric_command"]
                 mongo_dict["data"] = i["data"]
@@ -37,30 +38,30 @@ class Alerts():
         else:
             if metric_name is None:
                 mongo_list = []
-                for i in alerts_metrics.find({"vnf_id": vnf_id}):
+                for i in alerts_metrics.find({"xnf_id": xnf_id}):
                     mongo_dict = {
-                        "vnf-id": "",
+                        "xnf-id": "",
                         "metric-name": "",
                         "metric-command": "",
                         "data": "",
                         }
-                    mongo_dict["vnf-id"] = i["vnf_id"]
+                    mongo_dict["xnf-id"] = i["xnf_id"]
                     mongo_dict["metric-name"] = i["metric_name"]
                     mongo_dict["metric-command"] = i["metric_command"]
                     mongo_dict["data"] = i["data"]
                     mongo_list.append(mongo_dict)
                 return mongo_list
             else:
-                if vnf_id is None:
+                if xnf_id is None:
                     mongo_list = []
                     for i in alerts_metrics.find({"metric_name": metric_name}):
                         mongo_dict = {
-                            "vnf-id": "",
+                            "xnf-id": "",
                             "metric-name": "",
                             "metric-command": "",
                             "data": "",
                             }
-                        mongo_dict["vnf-id"] = i["vnf_id"]
+                        mongo_dict["xnf-id"] = i["xnf_id"]
                         mongo_dict["metric-name"] = i["metric_name"]
                         mongo_dict["metric-command"] = i["metric_command"]
                         mongo_dict["data"] = i["data"]
@@ -68,14 +69,14 @@ class Alerts():
                     return mongo_list
                 else:
                     mongo_list = []
-                    for i in alerts_metrics.find({"vnf_id": vnf_id, "metric_name": metric_name}):
+                    for i in alerts_metrics.find({"xnf_id": xnf_id, "metric_name": metric_name}):
                         mongo_dict = {
-                            "vnf-id": "",
+                            "xnf-id": "",
                             "metric-name": "",
                             "metric-command": "",
                             "data": "",
                             }
-                        mongo_dict["vnf-id"] = i["vnf_id"]
+                        mongo_dict["xnf-id"] = i["xnf_id"]
                         mongo_dict["metric-name"] = i["metric_name"]
                         mongo_dict["metric-command"] = i["metric_command"]
                         mongo_dict["data"] = i["data"]
