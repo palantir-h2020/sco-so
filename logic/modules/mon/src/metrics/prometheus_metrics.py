63d766b68f04a8c88b982ac61320fc058e884dd4
Cesar Cajas <cesar.cajas@i2cat.net>
2022-09-23 19:19:36 +0200

    code fix error

--- logic/modules/mon/src/metrics/prometheus_metrics.py
+++ logic/modules/mon/src/metrics/prometheus_metrics.py
@@ -30,46 +30,47 @@ class PrometheusMetrics():
         )
 
     def prometheus_metrics(self, request):
-        # Obtain values from request
-        request_body = request.json
-        target_id = request_body.get("xnf-id")
-        instance = request_body.get("xnf-ip")
-        metric_name = request_body.get("metric-name")
-        
-        targets_list = PrometheusTargets.objects.order_by("-id")\
-            .first().targets
-        if target_id in targets_list:
-        # Execute the request
-            self.client_query = "{}{}".format(
-                self.prometheus_query_endpoint, metric_name)
-            try:
-                metrics_data = requests.get(self.client_query)
-                metrics_json = metrics_data.json()
-                result = metrics_json["data"]["result"]
-                # Obtain xnf_index
-                xnf_list = []
-                for i in result:
-                    xnf_list.append(i["metric"]["instance"])
-                xnf_index = xnf_list.index(target_id)
-                # Create metrics_dict
-                metrics_dict = {
-                    "xnf-id": target_id,
-                    "metric-name": metric_name,
-                    "metric-value": "",
-                }
-                try:
-                    metric_value = result[xnf_index]["value"]
-                    metrics_dict["metric-value"] = metric_value
+            # Obtain values from request
+            request_body = request.json
+            target_id = request_body.get("xnf-id")
+            instance = request_body.get("xnf-ip")
+            metric_name = request_body.get("metric-name")
 
+            targets_list = PrometheusTargets.objects.order_by("-id")\
+                .first().targets
+            if target_id in targets_list:
+                # Execute the request
+                self.client_query = "{}{}".format(
+                    self.prometheus_query_endpoint, metric_name)
+                try:
+                    metrics_data = requests.get(self.client_query)
+                    metrics_json = metrics_data.json()
+                    result = metrics_json["data"]["result"]
+                    # Obtain xnf_index
+                    xnf_list = []
+                    for i in result:
+                        xnf_list.append(i["metric"]["instance"])
+                    xnf_index = xnf_list.index(target_id)
+                    # Create metrics_dict
+                    metrics_dict = {
+                        "xnf-id": target_id,
+                        "metric-name": metric_name,
+                        "metric-value": "",
+                    }
+                    try:
+                        metric_value = result[xnf_index]["value"]
+                        metrics_dict["metric-value"] = metric_value
+
+                    except KeyError:
+                        # Print temporal, para pruebas.
+                        print("Cannot obtain given metric from instance={}"
+                            .format(instance))
+                        metrics_dict["metric-value"] = "N/A"
+                    return metrics_dict
                 except Exception:
-                    err_message = "Cannot obtain given metric from instance={}".format(instance)
-                    err = {"Error": err_message}
+                    err = {"Error": "Prometheus server is not running"}
+                    return err
+            else:
+                err = {"Error": "{} target is not registered".format(target_id)}
                 return err
-            except Exception:
-                err_message = "Prometheus server is not running"
-                err = {"Error": err_message}
-                return err
-        else:
-            err_message = "{} target is not registered".format(target_id)
-            err = {"Error": err_message}
-            return err
+
